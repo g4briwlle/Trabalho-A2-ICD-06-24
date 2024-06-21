4676fdaa9b4ebfdfdad49e05b146ca00cbdc59a0
gabrielle mascarelo <gabriellemascarelo@gmail.com>
2024-06-21 19:55:19 -0300

    comecinho da ai

--- mod_openai.py
+++ mod_openai.py
@@ -1,2 +1,24 @@
 """ Geração de Insights Usando a API da OpenAI """
 
+from openai import OpenAI
+
+
+client = OpenAI(api_key="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")
+
+def converte_comentarios_em_nota():
+    lista_notas = []
+
+    prompt = [{"role": "user", "content": f""]
+
+    response = client.chat.completions.create(
+        messages=prompt,
+        model="gpt-3.5-turbo-0125",
+        max_tokens=200,
+        temperature=1
+    )
+
+    
+    lista_notas.append[] = 
+
+    return response
+
