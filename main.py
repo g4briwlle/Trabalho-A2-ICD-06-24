a7934726a5e6ff19ada350068aa532042535f533
gabrielle mascarelo <gabriellemascarelo@gmail.com>
2024-06-25 16:21:50 -0300

    recomeçando openai

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from mod_scraping import *
 from mod_cleansing import *
-#from mod_openai import *
+from mod_openai import *
 from mod_excel import *
 
 pega_reviews()
--- mod_openai.py
+++ mod_openai.py
@@ -6,27 +6,11 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import pandas as pd
 
-client = OpenAI(api_key="")
 
 dataset = pd.read_csv("A2.csv")
 print(dataset)
 
-"""
-def converte_comentarios_em_nota(comentario):
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = openai_api_key
 
-    prompt = [{
-        "role": "user",
-        "content": "Resuma "
-            }]
-
-    response = client.chat.completions.create(
-        messages=prompt,
-        model="gpt-3.5-turbo-0125",
-        max_tokens=200,
-        temperature=1
-    )
-
-    Resposta_do_chat = response.choices[0].message.content
-
-def insights(lista):
-"""
