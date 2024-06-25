b07fad99a4e060a697af3b81c345ea8c408e8b1c
Sofia Monteiro <128311404+sofiaazeredo@users.noreply.github.com>
2024-06-25 19:08:03 -0300

    Update mod_openai.py

--- mod_openai.py
+++ mod_openai.py
@@ -11,26 +11,14 @@ chave_ = input("Coloque sua chave API_KEY:")
 
 client = OpenAI(api_key= chave_)
 
-dataset = pd.read_csv("A2.csv")
-
-# primeiro insight
-
-datavalues = dataset.values
-
-lista_comentarios = []
-for x in range(0, 226):
-    lista_comentarios.append(datavalues[x][1:])
-
-def classifica_sentimento(comment):
-    prompt = [{
+prompt = [{
         "role":"user", 
-        "content":f"classifique o sentimento da seguinte review sobre um filme específico: {comment} escolhendo uma das seguintes categorias: Detestei, Não gostei, Apático, Gostei, Amei",
+        "content":f'Você é um analista de review deixada por um espectador do filme Paris, Texas . Você precisa me retornar 3 classificações, nas categorias chamadas Sentimento, Propensão e Aspecto. Suas classificações serão retornadas sem pontuações ou complementos, respeitando a capitalização utilizada, separadas por vírgula e espaço somente. Categoria Sentimento: Classifique o sentimento que passa a "Review a ser analisada" entre as classificações Detestei, Não gostei, Apático, Gostei, Amei. Categoria Propensão: Classifique a propensão de ver o filme novamente de alguém que escreveu a "Review a ser analisada" entre as classificações Veria Novamente, Não Veria Novamente. Categoria Aspecto: Classifique o assunto principal da "Review a ser analisada" escolhendo uma das seguintes categorias: Roteiro, Fotografia, Originalidade, Atuação, Emoção. Review a ser analisada: {comment}\nCategory:'
         }]
 
     response = client.chat.completions.create(
-        # engine="text-davinci-003",
         model = 'gpt-3.5-turbo-0125',
-        messages = prompt,
+        messages = prompt ,
         max_tokens = 600,
         temperature = 1
     )
@@ -38,63 +26,3 @@ def classifica_sentimento(comment):
     Resposta_do_chat = response.choices[0].message.content
 
     return Resposta_do_chat
-
-sentimentos = []
-
-for comentario in lista_comentarios:
-    print("ok")
-    sentimentos.append(classifica_sentimento(comentario))
-
-print(sentimentos)
-
-# segundo insight
-
-def classifica_categorias(comment):
-    prompt = [{
-        "role": "user",
-        "content": f"Classifique o assunto principal do comentário a seguir: {comment} escolhendo uma das seguintes categorias: roteiro, fotografia, originalidade, atuação, emoção"
-    }]
-    
-    response = client.chat.completions.create(
-        model='gpt-3.5-turbo-0125',
-        messages=prompt,
-        max_tokens=600,
-        temperature=1
-    )
-    
-    Resposta_do_chat = response.choices[0].message.content
-    
-    return Resposta_do_chat.strip()
-
-categorias = []
-
-for comentario in lista_comentarios:
-    categorias.append(classifica_categorias(comentario))
-
-print(categorias)
-
-# terceiro insight
-
-def classifica_propensao(comment):
-    prompt = [{
-        "role": "user",
-        "content": f"Classifique a propensão de alguém que escreveu o seguinte comentário sobre um filme: {comment} escolhendo uma das seguintes categorias: veria novamente, não veria novamente"
-    }]
-    
-    response = client.chat.completions.create(
-        model='gpt-3.5-turbo-0125',
-        messages=prompt,
-        max_tokens=600,
-        temperature=1
-    )
-    
-    Resposta_do_chat = response.choices[0].message.content
-    
-    return Resposta_do_chat.strip()
-
-propensoes = []
-
-for comentario in lista_comentarios:
-    propensoes.append(classifica_propensao(comentario))
-
-print(propensoes)
