04f1d3aae098bd70feed22e8e8e1d4d4f12ca270
gabrielle mascarelo <gabriellemascarelo@gmail.com>
2024-06-25 18:19:40 -0300

    terminando openai!

--- mod_openai.py
+++ mod_openai.py
@@ -1,18 +1,20 @@
-""" Geração de Insights Usando a API da OpenAI """
-
 import os
 import openai
 from openai import OpenAI
 from dotenv import load_dotenv
 import pandas as pd
 
-load_dotenv()
+# Load environment variables from the .env file
+load_dotenv('chave.env')
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = openai_api_key
-client = OpenAI(api_key="OPENAI_API_KEY")
+chave_ = input("Coloque sua chave API_KEY:")
+
+client = OpenAI(api_key= chave_)
 
 dataset = pd.read_csv("A2.csv")
+
+# primeiro insight
+
 datavalues = dataset.values
 
 lista_comentarios = []
@@ -22,14 +24,13 @@ for x in range(0, 226):
 def classifica_sentimento(comment):
     prompt = [{
         "role":"user", 
-        "content":f"classifique o sentimento da seguinte review sobre um filme específico: {comment}\nCategory:"        
+        "content":f"classifique o sentimento da seguinte review sobre um filme específico: {comment} escolhendo uma das seguintes categorias: Detestei, Não gostei, Apático, Gostei, Amei",
         }]
 
-    response = openai.completions.create(
-        engine="text-davinci-003",
+    response = client.chat.completions.create(
+        # engine="text-davinci-003",
         model = 'gpt-3.5-turbo-0125',
-        choices=["Detestei", "Não gostei", "Apático", "Gostei", "Amei"],
-        messages = prompt ,
+        messages = prompt,
         max_tokens = 600,
         temperature = 1
     )
@@ -41,6 +42,59 @@ def classifica_sentimento(comment):
 sentimentos = []
 
 for comentario in lista_comentarios:
+    print("ok")
     sentimentos.append(classifica_sentimento(comentario))
 
 print(sentimentos)
+
+# segundo insight
+
+def classifica_categorias(comment):
+    prompt = [{
+        "role": "user",
+        "content": f"Classifique o assunto principal do comentário a seguir: {comment} escolhendo uma das seguintes categorias: roteiro, fotografia, originalidade, atuação, emoção"
+    }]
+    
+    response = client.chat.completions.create(
+        model='gpt-3.5-turbo-0125',
+        messages=prompt,
+        max_tokens=600,
+        temperature=1
+    )
+    
+    Resposta_do_chat = response.choices[0].message.content
+    
+    return Resposta_do_chat.strip()
+
+categorias = []
+
+for comentario in lista_comentarios:
+    categorias.append(classifica_categorias(comentario))
+
+print(categorias)
+
+# terceiro insight
+
+def classifica_propensao(comment):
+    prompt = [{
+        "role": "user",
+        "content": f"Classifique a propensão de alguém que escreveu o seguinte comentário sobre um filme: {comment} escolhendo uma das seguintes categorias: veria novamente, não veria novamente"
+    }]
+    
+    response = client.chat.completions.create(
+        model='gpt-3.5-turbo-0125',
+        messages=prompt,
+        max_tokens=600,
+        temperature=1
+    )
+    
+    Resposta_do_chat = response.choices[0].message.content
+    
+    return Resposta_do_chat.strip()
+
+propensoes = []
+
+for comentario in lista_comentarios:
+    propensoes.append(classifica_propensao(comentario))
+
+print(propensoes)
