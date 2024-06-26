063ab3d36a020ccca154991ddf38e8ff7d802e67
Vinício Deusdará <160523695+viniciodeusdara@users.noreply.github.com>
2024-06-25 21:50:07 -0300

    Adicionando módulo que transfere dados extraídos pro Excel

--- mod_excel.py
+++ mod_excel.py
@@ -1,1 +1,26 @@
-""" nao sei se precisa mesmo de um arquivo py p lidar com a parte do excel mas caso sim ta aqui """
+""" Transferência das reviews para Excel """
+import pandas as pd
+from mod_openai import *
+from mod_cleansing import *
+
+reviews = cleansing('A2.csv')
+respostas = [gpt_classifica(review) for review in reviews]
+
+sentimento = []
+propensao = []
+aspecto = []
+
+for i in range(len(respostas)):
+    sentimento.append(respostas[i].split(",")[0])
+    propensao.append(respostas[i].split(",")[1])
+    aspecto.append(respostas[i].split(",")[2])
+
+data = {
+    'Sentimento': sentimento,
+    'Propensão': propensao,
+    'Aspecto': aspecto
+}
+
+df = pd.DataFrame(data)
+
+df.to_csv('reviews_tratadas.csv', index=False)
