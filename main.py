375ea540b6c29064f813d82aabc56ccacb90b9bd
Vinício Deusdará <160523695+viniciodeusdara@users.noreply.github.com>
2024-06-25 22:35:40 -0300

    Finalizando a main

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from mod_scraping import *
-from mod_cleansing import *
-from mod_openai import *
 from mod_excel import *
 
 pega_reviews()
-
-reviews = cleansing('A2.csv')
-
-respostas = [[gpt_classifica(review)] for review in reviews]
-
-print(respostas)
+exporta_reviews()
